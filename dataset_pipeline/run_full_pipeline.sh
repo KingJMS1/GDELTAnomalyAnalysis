2265bad2df8e9dcfd785a2a7093bccdcd96bae94
KingJMS1 <1kingjms@gmail.com>
2026-02-27 02:59:24 +0000

    Put a shebang

--- dataset_pipeline/run_full_pipeline.sh
+++ dataset_pipeline/run_full_pipeline.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 python download_gdelt.py
 python unzip.py  # This assumes you have ~16 cpu cores, if not, change the --jobs flag on parallel to a reasonable number instead of 8
 # At this point, all data has been converted to parquet. The next file, partition.py, filters down to the locations, countries, and event codes of interest. Edit that file to change our default lat/lon bounding box to what is desired for your study.
